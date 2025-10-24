28c9dc11166a813c56eebc44297c0842ac4ad896
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-10-24 10:07:48 +0200

    Allow note creation without note data (= empty note)

--- backend/src/api/routes/notes/create.ts
+++ backend/src/api/routes/notes/create.ts
@@ -5,15 +5,15 @@ import { ConflictError } from "../../error/classes";
 import { MaybeDefined, RequireDefined } from "../../params";
 
 const NotesCreateRoute = (async (req, res) => {
-  const [name, data] = RequireDefined<[string, string]>(req, "name", "data");
-  const [folderId] = MaybeDefined<[string?]>(req, "folderId");
+  const [name] = RequireDefined<[string, string]>(req, "name");
+  const [folderId, data] = MaybeDefined<[string?, string?]>(req, "folderId", "data");
   const user = await AssumeAuthorization(req);
 
   const existing = await GetNoteByName(user._id, name.toLowerCase(), folderId);
 
   if (existing) throw new ConflictError("A note with that name already exists in the specified folder.");
 
-  const note = await CreateNote(user._id, name, data, folderId);
+  const note = await CreateNote(user._id, name, data ?? "", folderId);
 
   res.json(note);
 }) satisfies RouteCallback;
