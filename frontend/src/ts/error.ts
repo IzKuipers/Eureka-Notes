1a009e680a2ab5285154f1f7550335d45ee751f9
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-02-06 11:46:59 +0100

    Check token expiry in client

--- frontend/src/ts/error.ts
+++ frontend/src/ts/error.ts
@@ -1,10 +1,32 @@
 import { AxiosError } from "axios";
 import { ShowDialog } from "./dialog";
+import { GlobalServerConnector } from "./api";
 
 export function globalErrorHandler(e: any) {
-  const axiosError = e instanceof AxiosError && (e as AxiosError);
+  const axiosError: AxiosError | false = e instanceof AxiosError && (e as AxiosError);
 
   if (axiosError) {
+    const data = axiosError.response?.data as { error?: string } | undefined;
+
+    // If the token has expired
+    if (data?.error?.startsWith("AuthorizationError") && data?.error?.includes("Invalid token")) {
+      ShowDialog({
+        title: "Login expired",
+        message: "Your token has expired because you didn't use eureka for too long. Please log in again.",
+        buttons: [
+          {
+            caption: "Reload",
+            action: () => {
+              GlobalServerConnector?.resetCookies()
+              location.reload();
+            },
+          },
+        ],
+      });
+
+      throw "";
+    }
+
     switch (axiosError.status) {
       case 401:
       case 400:
